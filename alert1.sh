5a591bd829366dfbf21c20520711fba48931549b
Phl00k <cphaggarty@gmail.com>
2016-06-07 12:54:40 -0700

    Update alert1.sh

--- alert1.sh
+++ alert1.sh
@@ -3,10 +3,10 @@
 ps aux | grep "blockdev --getsize64 /dev/sd*" | awk '{print $13}' >~/hlog.txt
 
 FILE=~/hlog.txt
-cnt=$(cat $FILE  | wc -l)
+cnt=$(cat $FILE | wc -l)
 
 if [ -f $FILE ]; then
-  echo "Updating and analyzing $FILE ..."
+  echo "Updating and scanning $FILE ..."
 if [ $cnt -gt 3 ]; then
   echo "Problem identified on $HOSTNAME... More than 3 returns... Sending alert emails..."
   echo "### Please Check $HOSTNAME. The above drive(s) are causing blockdev errors. Recommend Disk Replacement, Reboot if needed ###" >> $FILE
