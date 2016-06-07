4073a15ba81eb2ad0530512abf26df91ac38e2f2
Phl00k <cphaggarty@gmail.com>
2016-06-07 13:06:25 -0700

    Update alert1.sh

--- alert1.sh
+++ alert1.sh
@@ -10,7 +10,7 @@ if [ -f $FILE ]; then
 if [ $cnt -gt 3 ]; then
   echo "Problem identified on $HOSTNAME... More than 3 returns... Sending alert emails..."
   echo "### Please Check $HOSTNAME. The above drive(s) are causing blockdev errors. Recommend Disk Replacement, Reboot if needed ###" >> $FILE
-  echo "Subject: Problem on $HOSTNAME" | cat - $FILE | sendmail -F "$HOSTNAME" -t name@email.com
+  echo "Subject: Problem on $HOSTNAME" | cat - $FILE | sendmail -F "$HOSTNAME" -t chaggarty@pac-12.org
 else 
   echo "All systems normal!"
 fi ;
