0b2bfa6a617a23e34c9ff1faa720fd1b0f2dc608
Phl00k <cphaggarty@gmail.com>
2016-06-07 13:51:34 -0700

    Update alert1.sh

--- alert1.sh
+++ alert1.sh
@@ -11,7 +11,8 @@ if [ -f $FILE ]; then
   echo "Updating then scanning $FILE..."
 if [ $cnt -gt 3 ]; then
   echo "Problem identified on $HOSTNAME... More than 3 returns... Sending alert emails..."
-  echo "### Please Check $HOSTNAME. The above drive(s) are causing blockdev errors. Recommend Disk Replacement, Reboot if needed ###" >> $FILE
+  echo "##### Please Check $HOSTNAME. The drive(s) listed below are causing blockdev errors and are uninterruptible. Recommend actions: Disk Replacement ASAP, Reboot if users are reporting Tier3 retrieval errors. #####" | cat - $FILE > temp && mv temp $FILE
+  #echo "### Please Check $HOSTNAME. The above drive(s) are causing blockdev errors. Recommend Disk Replacement, Reboot if needed ###" >> $FILE
   echo "Subject: Problem on $HOSTNAME" | cat - $FILE | sendmail -F "$HOSTNAME" -t chaggarty@pac-12.org
 else 
   echo "All systems normal!"
