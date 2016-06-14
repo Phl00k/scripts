7caf05d6f97211616181de30318d23cb8c3ae174
Phl00k <cphaggarty@gmail.com>
2016-06-14 12:22:37 -0700

    Update OSTalert.sh

--- OSTalert.sh
+++ OSTalert.sh
@@ -11,7 +11,7 @@ if [ -f $FILE ]; then
   echo "Updating then scanning $FILE..."
 if [ $cnt -gt 3 ]; then
   echo "Problem identified on $HOSTNAME... $cnt returns... Sending alert emails..."
-  echo "##### Please Check $HOSTNAME. The drive(s) listed below are causing blockdev errors and are uninterruptible. Recommend actions: Disk Replacement ASAP, Reboot if users are reporting Tier3 retrieval errors. #####" | cat - $FILE > temp && mv temp $FILE
+  echo "##### Please Check $HOSTNAME. The drive(s) listed below are causing blockdev errors and are uninterruptible. Recommend actions: Disk Replacement ASAP, Reboot if users are reporting Tier3 retrieval errors. #####" | cat - $FILE > temp && mv -f temp $FILE
   echo "Subject: Problem on $HOSTNAME" | cat - $FILE | sendmail -F "$HOSTNAME" -t chaggarty@pac-12.org
 else 
   echo "All systems normal!"
